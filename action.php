ab912a19f4d22ec40adf141cdf7b93fa4040bd78
Jesse W <90267527+JesseLW19@users.noreply.github.com>
2021-09-10 17:10:05 -0400

    Update action.php

--- action.php
+++ action.php
@@ -1,41 +1,11 @@
-// Check if the form is submitted
-if ( isset( $_POST['submit'] ) ) {
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
 
-// retrieve the form data by using the element's name attributes value as key
-
-echo '<h2>form data retrieved by using the $_REQUEST variable<h2/>'
-
-$firstname = $_REQUEST['firstname'];
-$lastname = $_REQUEST['lastname'];
-
-// display the results
-echo 'Your name is ' . $lastname .' ' . $firstname;
-
-// check if the post method is used to submit the form
-
-if ( filter_has_var( INPUT_POST, 'submit' ) ) {
-
-echo '<h2>form data retrieved by using $_POST variable<h2/>'
-
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-
-// display the results
-echo 'Your name is ' . $lastname .' ' . $firstname;
-}
-
-// check if the get method is used to submit the form
-
-if ( filter_has_var( INPUT_GET, 'submit' ) ) {
-
-echo '<h2>form data retrieved by using $_GET variable<h2/>'
-
-$firstname = $_GET['firstname'];
-$lastname = $_GET['lastname'];
-}
-
-// display the results
-echo 'Your name is ' . $lastname .' ' . $firstname;
-exit;
-}
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
